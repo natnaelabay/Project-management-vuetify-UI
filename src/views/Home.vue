6ef407b99323b39c46cb5c3fc805b5b2fec71cf7
natnaelabay <se.natnael.abay@gmail.com>
2020-07-25 20:03:08 +0300

    starter

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <v-btn dark depressed="" color = "orange">asd asd </v-btn>
+    <v-btn dark>
+      <v-icon>email</v-icon>
+      <span>NAtnael</span>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
